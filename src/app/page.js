397850a214eb162049a8d20febb71461b6e6de79
Delewis Lawrence II <delewismlawrence@gmail.com>
2025-11-14 15:07:51 -0500

    add analytics again

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,24 @@
 "use client";
 import { useState } from "react";
-import { Analytics } from "@vercel/analytics/next"
-
-<Analytics />
+import { Analytics } from '@vercel/analytics/next';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
 function Square ({value, onClick}) {
 return <button className="square" onClick={onClick}>
 {value}
